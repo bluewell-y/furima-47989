f2ff6210b388c96dbfb6fb9f1af79c47546861c0
bluewell-y <aoiy04414@gmail.com>
2026-07-17 06:19:19 +0900

    Itemモデルの単体テストコードを実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ができるとき' do
+      it '必要な情報を適切に入力すると、商品情報がデータベースに保存される' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ができないとき' do
+      it '商品画像が必須であること' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('商品画像を入力してください')
+      end
+
+      it '商品名が必須であること' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('商品名を入力してください')
+      end
+
+      it '商品の説明が必須であること' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('商品の説明を入力してください')
+      end
+
+      it 'カテゴリーの情報が必須であること' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
+      end
+
+      it '商品の状態の情報が必須であること' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
+      end
+
+      it '配送料の負担の情報が必須であること' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
+      end
+
+      it '発送元の地域の情報が必須であること' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
+      end
+
+      it '発送までの日数の情報が必須であること' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
+      end
+
+      it '価格の情報が必須であること' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('価格を入力してください')
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
 end
