64b5768ff6440f1d3fd744b5803e4f471df87a9b
bluewell-y <aoiy04414@gmail.com>
2026-07-24 12:28:47 +0900

    商品情報編集機能のコントローラーとアクセス制限の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
-  # ログインしていないユーザーがnew（出品画面）にアクセスしたら、ログイン画面へリダイレクトする
-  before_action :authenticate_user!, only: [:new]
+  # ログインしていないユーザーが、出品・編集・更新などのログイン必須ページにアクセスした際、自動的にログイン画面へリダイレクトする
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # show, edit, updateアクションが実行される前に、リクエストされたIDの商品情報をDBから取得して @item に代入する
+  before_action :set_item, only: [:show, :edit, :update]
+  # ログイン中のユーザーが出品者本人でない場合、商品の編集・更新ページへのアクセスを拒否してトップページへリダイレクトする
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,7 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
   end
 
   private
@@ -28,5 +34,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
