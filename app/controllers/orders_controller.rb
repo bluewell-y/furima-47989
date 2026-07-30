cc52daadc0c98f88fa1684a57d8082f1b31ed91f
bluewell-y <aoiy04414@gmail.com>
2026-07-31 08:28:31 +0900

    アクセス制御とsold out表示を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
   def index
+    # 自身が出品した商品 または 売却済みの商品 の場合はトップページへ遷移
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+    @order_address = OrderAddress.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
-    @order_form = OrderForm.new
   end
 end
