ed09fc1484fc8aafec9d1baf03fd212b31841367
bluewell-y <aoiy04414@gmail.com>
2026-07-31 07:41:01 +0900

    Formオブジェクトの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
-class OrderForm
+class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building_name, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
