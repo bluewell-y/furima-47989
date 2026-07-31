6185c2cd4f7de9e89d2265ff66a7df20e6cdacb3
bluewell-y <aoiy04414@gmail.com>
2026-08-01 06:43:26 +0900

    エラー表示の設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,15 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選んでください' }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :phone_number
   end
+
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は『3桁ハイフン4桁』の半角数字で入力してください' }, allow_blank: true
+  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'は10桁以上11桁以内の半角数字（ハイフンなし）で入力してください' }, allow_blank: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
