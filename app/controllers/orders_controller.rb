15d4c306a63c6eb394efe0a6aa2ed99ac6e0161d
bluewell-y <aoiy04414@gmail.com>
2026-07-31 15:02:39 +0900

    購入フォームとパラメーター保存を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,17 +3,29 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    # 自身が出品した商品 または 売却済みの商品 の場合はトップページへ遷移
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 end
