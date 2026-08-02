5410aac0b30711ed3e5147c7f307e78672e5df8d
bluewell-y <aoiy04414@gmail.com>
2026-08-02 18:31:38 +0900

    エラーメッセージの表示順を修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,17 +2,19 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :postal_code
-    validates :prefecture_id, numericality: { other_than: 1, message: 'を選んでください' }
-    validates :city
-    validates :addresses
-    validates :phone_number
-  end
+  validates :user_id, presence: true
+  validates :item_id, presence: true
 
+  validates :postal_code, presence: true
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は『3桁ハイフン4桁』の半角数字で入力してください' }, allow_blank: true
+
+  validates :prefecture_id, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選んでください' }
+
+  validates :city, presence: true
+  validates :addresses, presence: true
+
+  validates :phone_number, presence: true
   validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'は10桁以上11桁以内の半角数字（ハイフンなし）で入力してください' }, allow_blank: true
 
   def save
