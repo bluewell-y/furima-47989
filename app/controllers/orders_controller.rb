611e53363a9c40ad27eb8c556a3fb8d857159b4c
bluewell-y <aoiy04414@gmail.com>
2026-08-03 13:23:03 +0900

    Rubocopの指摘に対応(order_paramsの行の長さを修正)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,9 +25,9 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token).merge(
-      user_id: current_user.id, item_id: params[:item_id]
-    )
+    params.require(:order_address)
+          .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
